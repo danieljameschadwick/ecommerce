f96fc5e84f36ed9363fd3d6bdd91825e354857d5
Daniel Chadwick <djchadwick@outlook.com>
2021-03-21 11:30:31 +0000

    ECOM Fixing page size, Product page (layout, product gallery styling)

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,23 +1,45 @@
 import { MOCK_PRODUCT } from "../../enumeration/MockValues";
-
+import ImageGallery from "react-image-gallery";
+ 
 type IProps = {
     id: number;
     name: string;
 };
 
+const images = [
+    {
+        original: MOCK_PRODUCT.IMAGE_PATH,
+        thumbnail: MOCK_PRODUCT.IMAGE_PATH,
+    },
+    {
+        original: MOCK_PRODUCT.IMAGE_PATH,
+        thumbnail: MOCK_PRODUCT.IMAGE_PATH,
+    },
+    {
+        original: MOCK_PRODUCT.IMAGE_PATH,
+        thumbnail: MOCK_PRODUCT.IMAGE_PATH,
+    },
+];
+
 const Product = ({ id, name }: IProps) => {
+    const showThumbnails = images.length > 1;
+
     return (
         <div className={"container"}>
-            <div className={'flex flex-nowrap'}>
-                <div className={'w-2/3 mr-5'}>
-                    <picture className={'w-full h-full flex max-h-600'}>
-                        <source srcSet={MOCK_PRODUCT.IMAGE_PATH}
-                            media={"(min-width: 800px)"} />
-                        <img className={'w-full object-cover'} src={MOCK_PRODUCT.IMAGE_PATH} alt={MOCK_PRODUCT.IMAGE_ALT_TEXT} />
-                    </picture>
+            <div className={'flex flex-wrap sm:flex-nowrap'}>
+                <div className={'w-full sm:w-2/3 sm:mr-5'}>
+                    <ImageGallery 
+                        additionalClass={"product-gallery"}
+                        items={images} 
+                        showNav={false}
+                        showPlayButton={false}
+                        showBullets={false}
+                        showThumbnails={showThumbnails}
+                        thumbnailPosition={"bottom"}
+                    />
                 </div>
 
-                <div className={'w-1/3 flex flex-col'}>
+                <div className={"w-full sm:w-1/3 flex flex-col"}>
                     <h1>{MOCK_PRODUCT.NAME}</h1>
 
                     <p>
@@ -26,7 +48,9 @@ const Product = ({ id, name }: IProps) => {
                 </div>
             </div>
 
-           
+           <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque luctus, erat id rhoncus pharetra, velit dui dapibus risus, a euismod augue nisi vel neque. Donec pellentesque, elit vitae dignissim dictum, magna eros vulputate ante, ut dapibus urna tellus quis purus. Fusce eget sodales augue, quis congue tortor. Vestibulum consequat efficitur justo et mattis. Sed euismod tristique porta. Integer aliquam eget ligula a semper. Quisque ut posuere quam. Sed vestibulum pharetra justo, non semper sapien. Proin imperdiet eu orci eget luctus. Praesent aliquam lectus ac leo tincidunt, quis vulputate ante porta.
+           </div>
         </div>
     );
 };
