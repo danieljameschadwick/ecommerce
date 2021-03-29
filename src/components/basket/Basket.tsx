5a30aea646522681d26139ac72d73f172af53530
Daniel Chadwick <djchadwick@outlook.com>
2021-03-29 22:14:28 +0100

    ECOM Basket pagr (list products), removing debug

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -1,11 +1,39 @@
 import { useSelector } from "react-redux";
+import { Basket as PersistedBasket } from "../../util/state/Basket";
 
 export const Basket: React.FC = () => {
-    const basket = useSelector(state => state.basket);
+    const basket: PersistedBasket = useSelector(state => state.basket.basket);
+
+    if (undefined === basket) {
+        return (
+            <div>No basket???</div>
+        );
+    }
+
+    const products: Product[] = [];
+
+    // todo: add unhashed getProducts to Basket
+    for (const keyMap in basket.products) {
+        const productMap = basket.products[keyMap]
+
+        for (const key in productMap) {
+            const product = productMap[key];
+
+            products.push(product);
+        }
+    }
 
     return (
         <div>
             <h1>Basket</h1>
+
+            {products.map(product => {
+                return (
+                    <div className={"text-white-base"}>
+                        {product.getSku()}, {product.quantity}
+                    </div>
+                );
+            })}
         </div>
     )
 };
